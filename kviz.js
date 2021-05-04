77d4026753d1286ad59a54aa8c2cd2bb2b3d7d56
karolyim <matej.karolyi@gmail.com>
2021-05-04 06:05:17 +0200

    [MAINTENANCE] hint added

---
+++ kviz.js
@@ -0,0 +1,19 @@
+// Nejdříve si vytvoř objekt, který bude držet voje super otázky :-)
+
+const otazky = [];
+
+// Dále budeš potřebovat další proměnné - jaké?
+
+
+// Tato funkce se postará o vygenerování otázky
+// Zavoláme ji jednou na začátku a poté vždy po odpovězení
+function zobrazOtazku() {}
+
+// Funkce se postará o obsluhu kliknutí na odpověď
+// Musíme ji navázat na kokrétní odpovědi každé otázky (to uděláme v rámci funkce zobrazOtazku())
+function klikNaOdpoved() {}
+
+// Když už mám odpovězeno na vše (řídí se velikosí objektu otazky na řádku 3), tak mohu zobrazi výsledky
+// Vypočítám skóre a nageneruje nové elementy do HTML
+// Touto funkcí končí můj program (budu se rozhodovat, zda ji zavolat v rámci klikNaOdpoved())
+function zobrazVyhodnoceni() {}
