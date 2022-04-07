11726fa3e55600aa3034b410da4798caa238f023
Zuzana Volfova <zuzka.volfova@gmail.com>
2022-04-07 20:07:04 +0200

    edit function novaOtazka change condition

--- kviz.js
+++ kviz.js
@@ -22,14 +22,12 @@ let indexOtazky = 0;
 
 function novaOtazka(){
 
-    //pricist 1 k poradi otazky
-
-
-   
-
-    //kdyz je poradi otazky vetsi nez 3 zobraz vysledky
-    if (indexOtazky === otazky.length){
-        let kviz = document.querySelector('.kviz');
+    if (indexOtazky < otazky.length){
+        document.querySelector('#poradiOtazky').textContent = " "+ (indexOtazky+1);
+        document.querySelector('#otazka').textContent = otazky[indexOtazky].kvizOtazka;
+        indexOtazky += 1;
+    }
+    else {let kviz = document.querySelector('.kviz');
         let obsah = document.querySelector('.obsah');
         kviz.removeChild(obsah);
         
@@ -38,11 +36,7 @@ function novaOtazka(){
         kviz.appendChild(vysledek);
         vysledek.innerHTML = 'pokus';
         // jmenoZvirete.className = 'jmeno'
-    }
-    else {
-        document.querySelector('#poradiOtazky').textContent = " "+ (indexOtazky+1);
-        document.querySelector('#otazka').textContent = otazky[indexOtazky].kvizOtazka;
-        indexOtazky += 1;
+        
     }
 }
 
